42f152701d0522ae992fecff5c796343fe40cf31
cwyllie <122704627+ChazWyllie@users.noreply.github.com>
2024-12-29 19:00:25 -0700

    Create text_logger.py

---
+++ file_io/projects/text_logger/text_logger.py
@@ -0,0 +1,8 @@
+from logger import TextLogger
+
+logger = TextLogger()
+
+logger.info("This is an info message")
+logger.warning("This is a warning message")
+logger.error("This is an error message")
+
