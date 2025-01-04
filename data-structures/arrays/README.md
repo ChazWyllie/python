65fdc827d2b91b6e97dbef7523b6add3e32d1afa
qwopper <forsaken0915@gmail.com>
2025-01-03 17:51:16 -0700

    Made changes to array implementation and README.md

---
+++ data-structures/arrays/README.md
@@ -0,0 +1,21 @@
+## Description
+Arrays are contiguous memory structures that store elements (usually of the same type)
+
+## Key Characteristics of Arrays
+- Index-based: Elements are accesed via indices (starting from 0)
+- Contigious memory: Elements are stored back-to-back in memory
+- Fixed size: In some languages, some arrays cannot change after creation
+- Dynamic (Python lists): Python listrs can expand or contract based on the operation
+
+## Basic Operations
+- Access: arr[index]
+- Insert: arr.append(value) or arr.insert(index, value)
+- Delete: del arr[index] or arr.pop(index)
+- Iteration: for i in arr:
+
+## Time Complexity of Array Operations
+- Access (by index) = O(1)
+- Search (unsorted) = O(n)
+- Insert (end) = O(1)
+- Insert (at index) = O(n)
+- Delete (by value) = O(n)
---
+++ data-structures/arrays/array_implementation.py
@@ -0,0 +1,48 @@
+# Description: Implementing an array in Python
+arr = [1, 2, 3, 4, 5]
+print(f"array list (arr): {arr}")
+
+# Accessing the first element
+print(f"first element of arr: {arr[0]}")
+
+# Accessing the last element
+print(f"last element of arr: {arr[-1]}")
+
+# Modifying the first element
+arr[0] = 10
+print(f"modified first element of arr: {arr[0]}")
+print(f"array list (arr) with index 0 modified: {arr}")
+
+# Inserting an element at the end
+arr.append(6)
+print(f"array list (arr) with 6 appended: {arr}")
+
+# Inserting an element at a specific index
+arr.insert(3, 7)
+print(f"array list (arr) with 7 inserted at index 3: {arr}")
+
+# Removing an element by value
+arr.remove(7)
+print(f"array list (arr) with 7 removed: {arr}")
+
+# Removing an element by index
+arr.pop(3)
+
+# Searching if an element exists in an array
+print(f"checking if 10 exists in arr: {10 in arr}")
+if 10 in arr:
+    print("Element exists")
+    
+# Traverse through the array
+print("Traversing through the array")
+for i in arr:
+    print(f"index {arr.index(i)}: {i}")
+
+# Finding the length of an array
+print(f"length of arr: {len(arr)}")
+
+# Finding the index of an element
+print(f"finding the index of an element 3: {arr.index(3)} is the position of 3 in arr")
+
+# Finding the count of an element
+print(f"finding the count of an element in arr 10: there are {arr.count(10)} 10s in arr")
